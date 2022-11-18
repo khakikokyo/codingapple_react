5494ea59cba02d40c4b55fc5e5ff8a4d2d16d253
khakikokyo <phyeyoung90@gmail.com>
2022-11-18 19:57:58 +0900

    state

--- blog/README.md
+++ blog/README.md
@@ -11,8 +11,8 @@ React blog 제작과 기본 문법에 대해 공부합니다.
 [Create React App](https://create-react-app.dev/) 라이브러리 -
 1. 작업용 폴더 생성
 2. 터미널
-```
-npx create-react-add 프로젝트명
+```bash
+$ npx create-react-add 프로젝트명
 ```
 3. vscode 프로젝트 폴더 오픈
 
@@ -40,3 +40,21 @@ let post = '강남 우동 맛집';
 ```
 
 # State
+state는 변동사항이 생기면 state를 사용하여 html도 `자동으로 재렌더링`
+1. UI 기능 개발 편리
+2. 부드러운 사이트 동작
+- `state` 만드는 방법
+```javascript
+// 1. import
+import { useState } from 'react'
+
+// 2. let[작명1, 작명2] = useState(보관할 자료)
+// 작명1-a: state에 보관했던 자료
+// 작명2-b: state 변경을 도와주는 함수
+let [a, b] = useState('남자 코트 추천');
+```
+- 참고) Destructuring 문법
+```javascript
+let num = [1, 2];
+let [a, b] = [1, 2];
+```
--- blog/src/App.js
+++ blog/src/App.js
@@ -1,18 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [postTitle, b] = useState(['남자코트 추천', '강남 우동맛집', '리액트독학']);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={ {color: 'red', fontSize: '20px'} }>PHY's BLOG</h4>
+        <h4>React Blog</h4>
       </div>
       <div className="list">
-        <h4>글제목</h4>
-        <p>2월 17일 발행</p>
+        <h4>{postTitle[0]}</h4>
+        <p>11월 18일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{postTitle[1]}</h4>
+        <p>11월 18일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{postTitle[2]}</h4>
+        <p>11월 18일 발행</p>
       </div>
     </div>
   );
